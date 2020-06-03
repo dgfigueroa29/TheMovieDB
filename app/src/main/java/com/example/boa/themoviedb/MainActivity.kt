b6a2e16a888b17f558603a8fdb460be418d8e688
David Figueroa <dgfigueroa29@gmail.com>
2020-06-03 17:27:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/boa/themoviedb/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.boa.themoviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.boa.themoviedb.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
